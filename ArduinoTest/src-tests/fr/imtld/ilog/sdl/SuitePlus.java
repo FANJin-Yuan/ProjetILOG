1a7d5aa1bd15261478a7d6701bcbdddb0fb312da
jortscaroff <jortscaroff@B12S01.imtld.local>
2019-05-14 11:22:11 +0200

    Added rxtx library SerialTest and RxTxTest

--- ArduinoTest/src-tests/fr/imtld/ilog/sdl/SuitePlus.java
+++ ArduinoTest/src-tests/fr/imtld/ilog/sdl/SuitePlus.java
@@ -1,10 +1,6 @@
 package fr.imtld.ilog.sdl;
 
-import java.util.List;
-
-import org.junit.runner.Description;
 import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
 import org.junit.runner.Runner;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.Suite;
@@ -12,8 +8,6 @@ import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.RunnerBuilder;
 
 public class SuitePlus extends Suite {
-
-	Result result;
 	
 	public SuitePlus(Class<?> klass, RunnerBuilder builder) throws InitializationError {
 		super(klass, builder);
