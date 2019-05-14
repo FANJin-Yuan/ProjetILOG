c947d3a33b178a7b8d4e538f8a792e3b20b29222
Jaime <jortscaroff@gmail.com>
2019-05-14 21:51:48 +0200

    Working display in main

--- src-tests/test/AllTests.java
+++ src-tests/test/AllTests.java
@@ -2,6 +2,9 @@ package test;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
@@ -9,51 +12,48 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import gnu.io.CommPort;
-import gnu.io.CommPortIdentifier;
-import gnu.io.NoSuchPortException;
-import gnu.io.PortInUseException;
-import gnu.io.SerialPort;
-import gnu.io.UnsupportedCommOperationException;
-import junit.framework.Test;
-
+import com.fazecast.jSerialComm.SerialPort;
 
 @RunWith(SuitePlus.class)
 @SuiteClasses({ FifoTest.class, FifoTest2.class })
 public class AllTests {
-	
+
 	static SerialPort serialPort;
-	
+
 	public static void main(String[] args) throws Exception {
 
 		Result result = JUnitCore.runClasses(AllTests.class);
-		
-		CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier("COM4");
-        if ( portIdentifier.isCurrentlyOwned() )
-        {
-            System.out.println("Error: Port is currently in use");
-        }
-        else
-        {
-            CommPort commPort = portIdentifier.open("Appli",2000);
-            
-            if ( commPort instanceof SerialPort )
-            {
-                SerialPort serialPort = (SerialPort) commPort;
-                serialPort.setSerialPortParams(9600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
-                
-                serialPort.setDTR(false);
-                AllTests.serialPort = serialPort;           
-                OutputStream out = serialPort.getOutputStream();
-            }
-            else
-            {
-                System.out.println("Error: Only serial ports are handled.");
-            }
-        }
 
-		
-		
+		// attempt to connect to the serial port
+		serialPort = SerialPort.getCommPort("COM3");
+		serialPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
+		if (serialPort.openPort()) {
+
+			// create a new thread for sending data to the arduino
+			Thread thread = new Thread() {
+				@Override
+				public void run() {
+					// wait after connecting, so the bootloader can finish
+					try {
+						Thread.sleep(100);
+					} catch (Exception e) {
+					}
+
+					// enter an infinite loop that sends text to the arduino
+					PrintWriter output = new PrintWriter(serialPort.getOutputStream());
+					while (true) {
+						output.print(result.wasSuccessful());
+						output.flush();
+						try {
+							Thread.sleep(100);
+						} catch (Exception e) {
+						}
+					}
+				}
+			};
+			thread.start();
+
+		}
+
 	}
-
 }
