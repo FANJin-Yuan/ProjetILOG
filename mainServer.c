14f956dafab83d82c81d8a680d39232d46e271a1
gabriel <gabriel@192.168.1.95>
2019-05-14 14:55:54 +0200

    mainServer avec boucle pour rejouer

--- mainServer.c
+++ mainServer.c
@@ -15,6 +15,7 @@
 #include <netinet/ip.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <string.h>
 
 
 
@@ -23,46 +24,56 @@ int main(int argc, const char * argv[]) {
 	// Creation d'un entier aléatoire
 	srandom(time(NULL));
 	int nombreaTrouver = random()%1000;
+	int gagne = 0;
 	printf("%d\n",nombreaTrouver);
 
 	//Creation Serveur
 	int aSock;
-    aSock=socket(AF_INET,SOCK_STREAM,0);
-    struct sockaddr_in aSock_serv;
-    aSock_serv.sin_family=AF_INET;
-    aSock_serv.sin_port=htons(49152);
-    aSock_serv.sin_addr.s_addr=htonl(INADDR_ANY);
-    int err = 0;
-    err = bind (aSock, (struct sockaddr *)&aSock_serv, sizeof(aSock_serv));
+	aSock=socket(AF_INET,SOCK_STREAM,0);
+	struct sockaddr_in aSock_serv;
+	aSock_serv.sin_family=AF_INET;
+	aSock_serv.sin_port=htons(49152);
+	aSock_serv.sin_addr.s_addr=htonl(INADDR_ANY);
+	int err = 0;
+	err = bind (aSock, (struct sockaddr *)&aSock_serv, sizeof(aSock_serv));
 
-    //Le serveur est définit en écoute
-    err = listen(aSock, 10);
-    socklen_t casock = sizeof(aSock_serv);
+	//Le serveur est définit en écoute
+	err = listen(aSock, 10);
+	socklen_t casock = sizeof(aSock_serv);
 
-    // Bloque en attente de connection par un client
-    int the_Sock = accept(aSock, (struct sockaddr *)&aSock_serv, &casock);
-    char msgRecu[256];
+	// Bloque en attente de connection par un client
+	int the_Sock = accept(aSock, (struct sockaddr *)&aSock_serv, &casock);
+	char msgRecu[256];
+	while (gagne==0){
+		printf("passe\n");
+		// Bloque en Attente de reception
+		long aNum = recv(the_Sock, msgRecu, 256, 0);
+		printf ("%s", msgRecu);
+		int nombreRecu = atoi(msgRecu);
+		printf("%d",nombreRecu);
 
-    // Bloque en Attente de reception
-    long aNum = recv(the_Sock, msgRecu, 256, 0);
-    printf ("%s", msgRecu);
-    int nombreRecu = atoi(msgRecu);
+		//Comparaison nombreRecu et nombreaTrouver
+		char * msgEnvoye;
+		msgEnvoye = malloc(sizeof msgEnvoye);
+		if (nombreRecu==-1){
+			gagne=1;
+		}
+		else if (nombreRecu < nombreaTrouver){
+			msgEnvoye = "Plus  !";
+			printf("%d",nombreRecu);
+		} else if (nombreRecu > nombreaTrouver) {
+			msgEnvoye = "Moins !";
+		} else {
+			msgEnvoye = "Gagne !";
+			gagne=1;
+		}
+		printf("%s",msgEnvoye);
+		send (the_Sock,msgEnvoye,strlen(msgEnvoye),0);
+	}
 
-    //Comparaison nombreRecu et nombreaTrouver
-    char * msgEnvoye;
-    msgEnvoye = malloc(sizeof msgEnvoye);
-    if (nombreRecu < nombreaTrouver){
-    	msgEnvoye = "Plus  !";
-    } else if (nombreRecu > nombreaTrouver) {
-    	msgEnvoye = "Moins !";
-    } else {
-    	msgEnvoye = "Gagne !";
-    }
-    printf("%s",msgEnvoye);
+	//Fermeture du server
+	close (aSock);
+	close (the_Sock);
 
-    //Fermeture du server
-    close (aSock);
-    close (the_Sock);
-
-    return 0;
+	return 0;
 }
