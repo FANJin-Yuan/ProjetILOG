21bdb33bf459a2d60f35623de69a3ad64ecc7722
gabriel <gabriel@192.168.1.95>
2019-05-15 11:04:26 +0200

    Client peut perdre et Stop du server et de tous les client quand l un d eux gagne

--- client.c
+++ client.c
@@ -16,61 +16,66 @@ void *print_message_function( void *ptr );
 
 int main()
 {
-     pthread_t thread1;
-     char *message1 = "Thread 1";
-     int  iret1;
-    /* Create independent threads each of which will execute function */
+	pthread_t thread1;
+	char *message1 = "Thread 1";
+	int  iret1;
+	/* Create independent threads each of which will execute function */
 
-     iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
+	iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
 
-     /* Wait till threads are complete before main continues. Unless we  */
-     /* wait we run the risk of executing an exit which will terminate   */
-     /* the process and all threads before the threads have completed.   */
+	/* Wait till threads are complete before main continues. Unless we  */
+	/* wait we run the risk of executing an exit which will terminate   */
+	/* the process and all threads before the threads have completed.   */
 
-     pthread_join( thread1, NULL);
+	pthread_join( thread1, NULL);
 
-     printf("Thread 1 returns: %d\n",iret1);
-     exit(0);
-     return 0;
+	printf("Thread 1 returns: %d\n",iret1);
+	exit(0);
+	return 0;
 }
 
 void *print_message_function( void *ptr )
 {
-     char *message;
-     message = (char *) ptr;
-     printf("%s \n", message);
-     struct hostent *hostinfo = NULL;
-     struct sockaddr_in Socka_imp = { 0 };
-     const char *hostname ="127.0.0.1";
-     int aSock;
-     hostinfo = gethostbyname(hostname);
-     aSock=socket(AF_INET,SOCK_STREAM,0);
-     Socka_imp.sin_family=AF_INET;
-     Socka_imp.sin_port=htons(49152);
-     Socka_imp.sin_addr= *(struct in_addr *) hostinfo->h_addr;
-     int err = connect(aSock, (struct sockaddr *)&Socka_imp, sizeof(struct sockaddr));
-     printf("err : %d \n",err);
-     if (err!=-1) {
-       int valread;
-       int nb;
-       int gagne = 0;
-       while (gagne == 0) {
-         char buffer[256] = {0};
-         printf("Donner un nombre : ");
-         scanf("%d", &nb);
-         printf("Nombre choisi : %d \n", nb);
-         char strnb[256] = {0};
-         sprintf(strnb, "%d", nb);
-         printf("Nombre en char : %s \n", strnb);
-         send (aSock,&strnb,strlen(&strnb),0);
-         printf("Test \n");
-         valread = read( aSock , buffer, 256);
-         printf("Valeur lu : %d \n", valread);
-         printf("Buffer recu : %s \n", buffer);
-         if (buffer[0] == 71) {
-           gagne = 1;
-         }
-       }
-       close(aSock);
-     }
+	char *message;
+	message = (char *) ptr;
+	printf("%s \n", message);
+	struct hostent *hostinfo = NULL;
+	struct sockaddr_in Socka_imp = { 0 };
+	const char *hostname ="127.0.0.1";
+	int aSock;
+	hostinfo = gethostbyname(hostname);
+	aSock=socket(AF_INET,SOCK_STREAM,0);
+	Socka_imp.sin_family=AF_INET;
+	Socka_imp.sin_port=htons(49152);
+	Socka_imp.sin_addr= *(struct in_addr *) hostinfo->h_addr;
+	int err = connect(aSock, (struct sockaddr *)&Socka_imp, sizeof(struct sockaddr));
+	printf("err : %d \n",err);
+	if (err!=-1) {
+		int valread;
+		int nb;
+		int gagne = 0;
+		while (gagne == 0) {
+			char buffer[256] = {0};
+			printf("Donner un nombre : ");
+			scanf("%d", &nb);
+			printf("Nombre choisi : %d \n", nb);
+			char strnb[256] = {0};
+			sprintf(strnb, "%d", nb);
+			printf("Nombre en char : %s \n", strnb);
+			int erreur = send (aSock,&strnb,strlen(&strnb),0);
+			printf("Test \n");
+			valread = read( aSock , buffer, 256);
+			if (valread!=7){
+				printf("Perdu !");
+				gagne=1;
+			}else{
+				printf("Valeur lu : %d \n", valread);
+				printf("Buffer recu : %s \n", buffer);
+				if (buffer[0] == 71) {
+					gagne = 1;
+				}
+			}
+		}
+		close(aSock);
+	}
 }
--- mainServer.c
+++ mainServer.c
@@ -17,16 +17,33 @@
 #include <netdb.h>
 #include <string.h>
 
+int finServer=0;
 int gagne = 0;
 void *processJeuServer(void *arg);
+void *thrServer(void *arg);
+static pthread_mutex_t mutex_stock;
 
 
 int main(int argc, const char * argv[]) {
 
+	pthread_mutex_init(&mutex_stock,0);
 	// Creation d'un entier aléatoire
 	srandom(time(NULL));
 	int nombreaTrouver = random()%1000;
 	printf("%d\n",nombreaTrouver);
+
+	pthread_t thread1;
+	int iret1;
+	iret1 = pthread_create( &thread1, NULL, thrServer, (void*) nombreaTrouver);
+	pthread_join( thread1, NULL);
+	pthread_mutex_destroy(&mutex_stock);
+	printf("Thread 1 returns: %d\n",iret1);
+	exit(0);
+	return 0;
+}
+
+void *thrServer (void *arg){
+	int *nombreaTrouver = arg;
 
 	//Creation Serveur
 	int aSock;
@@ -41,31 +58,22 @@ int main(int argc, const char * argv[]) {
 	//Le serveur est définit en écoute
 	err = listen(aSock, 10);
 	socklen_t casock = sizeof(aSock_serv);
+	int the_Sock;
+	while(finServer==0){
+		// Bloque en attente de connection par un client
+		the_Sock = accept(aSock, (struct sockaddr *)&aSock_serv, &casock);
+		pthread_t thread1;
 
-	// Bloque en attente de connection par un client
-	int the_Sock = accept(aSock, (struct sockaddr *)&aSock_serv, &casock);
-	pthread_t thread1;
-
-	char *message1 = "Thread 1";
-	int  iret1;
-	/* Create independent threads each of which will execute function */
-	int arg[2] = {the_Sock, nombreaTrouver};
-	iret1 = pthread_create( &thread1, NULL, processJeuServer, (void*) arg);
-
-	/* Wait till threads are complete before main continues. Unless we  */
-	/* wait we run the risk of executing an exit which will terminate   */
-	/* the process and all threads before the threads have completed.   */
-
-	pthread_join( thread1, NULL);
-
-	printf("Thread 1 returns: %d\n",iret1);
-	exit(0);
-
+		char *message1 = "Thread 1";
+		int  iret1;
+		/* Create independent threads each of which will execute function */
+		int arg[2] = {the_Sock, nombreaTrouver};
+		iret1 = pthread_create( &thread1, NULL, processJeuServer, (void*) arg);
+		printf("Thread 1 returns: %d\n",iret1);
+	}
 	//Fermeture du server
 	close (aSock);
 	close (the_Sock);
-
-	return 0;
 }
 
 void *processJeuServer(void *arg){
@@ -75,31 +83,35 @@ void *processJeuServer(void *arg){
 	printf("%d",the_Sock);
 	printf("%d",nombreaTrouver);
 	while (gagne==0){
-			char msgRecu[256] = {0};
-			printf("passe\n");
-			// Bloque en Attente de reception
-			long aNum = recv(the_Sock, msgRecu, 256, 0);
-			printf ("%s", msgRecu);
-			int nombreRecu = atoi(msgRecu);
-			printf("%d",nombreRecu);
+		char msgRecu[256] = {0};
+		printf("passe\n");
+		// Bloque en Attente de reception
+		long aNum = recv(the_Sock, msgRecu, 256, 0);
+		printf ("%s", msgRecu);
+		int nombreRecu = atoi(msgRecu);
+		printf("%d",nombreRecu);
 
-			//Comparaison nombreRecu et nombreaTrouver
-			char * msgEnvoye;
-			msgEnvoye = malloc(sizeof msgEnvoye);
-			if (nombreRecu==-1){
-				gagne=1;
-			}
-			else if (nombreRecu < nombreaTrouver){
-				msgEnvoye = "Plus  !";
-				printf("%d",nombreRecu);
-			} else if (nombreRecu > nombreaTrouver) {
-				msgEnvoye = "Moins !";
-			} else {
-				msgEnvoye = "Gagne !";
-				gagne=1;
-			}
-			printf("%s",msgEnvoye);
-			send (the_Sock,msgEnvoye,strlen(msgEnvoye),0);
+		//Comparaison nombreRecu et nombreaTrouver
+		char * msgEnvoye;
+		msgEnvoye = malloc(sizeof msgEnvoye);
+		if (nombreRecu==-1){
+			gagne=1;
 		}
+		else if (nombreRecu < nombreaTrouver){
+			msgEnvoye = "Plus  !";
+			printf("%d",nombreRecu);
+		} else if (nombreRecu > nombreaTrouver) {
+			msgEnvoye = "Moins !";
+		} else {
+			msgEnvoye = "Gagne !";
+			gagne=1;
+			finServer=1;
+		}
+		printf("%s",msgEnvoye);
+		send (the_Sock,msgEnvoye,strlen(msgEnvoye),0);
+		if (finServer==1){
+			exit(0);
+		}
+	}
 
 }
