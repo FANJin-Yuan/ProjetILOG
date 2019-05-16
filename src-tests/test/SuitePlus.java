f796c1ac92d3b82e00506eebf6257383de849491
Jaime <jortscaroff@gmail.com>
2019-05-16 10:09:54 +0200

    Updated SuitePlus runChild method, functional

--- src-tests/test/SuitePlus.java
+++ src-tests/test/SuitePlus.java
@@ -2,10 +2,7 @@ package test;
 
 import java.io.PrintWriter;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
 
 import org.junit.AssumptionViolatedException;
 import org.junit.internal.runners.model.EachTestNotifier;
@@ -26,6 +23,7 @@ public class SuitePlus extends Suite {
 	static int iTestNb;
 	static ArrayList<String> listTests = new ArrayList<String>();
 	static ArrayList<Result> listResults = new ArrayList<Result>();
+	static String message = "";
 
 	public SuitePlus(Class<?> klass, RunnerBuilder builder) throws InitializationError {
 		super(klass, builder);
@@ -42,19 +40,16 @@ public class SuitePlus extends Suite {
 			e1.printStackTrace();
 		}
 
-
-
-			Annotation[] arrAnnotation = AllTests.class.getDeclaredAnnotations();
-			for (Annotation annotation : arrAnnotation) {
-				System.out.println(annotation.toString());
-				System.out.println(annotation.annotationType().toString());
-				if (annotation.annotationType().toString().equals("interface org.junit.runners.Suite$SuiteClasses")) {
-					SuiteClasses testSuiteAnnotation = (SuiteClasses) annotation;
-					iTestNb = testSuiteAnnotation.value().length;
-				}
-
+		Annotation[] arrAnnotation = AllTests.class.getDeclaredAnnotations();
+		for (Annotation annotation : arrAnnotation) {
+			System.out.println(annotation.toString());
+			System.out.println(annotation.annotationType().toString());
+			if (annotation.annotationType().toString().equals("interface org.junit.runners.Suite$SuiteClasses")) {
+				SuiteClasses testSuiteAnnotation = (SuiteClasses) annotation;
+				iTestNb = testSuiteAnnotation.value().length;
 			}
 
+		}
 
 		String testName = this.getDescription().toString();
 		System.out.println();
@@ -99,32 +94,39 @@ public class SuitePlus extends Suite {
 			try {
 				// attempt to connect to the serial port
 
-				/*
-				 * SerialPort serialPort = SerialPort.getCommPort("COM3");
-				 * serialPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0); if
-				 * (serialPort.openPort()) { PrintWriter output = new
-				 * PrintWriter(serialPort.getOutputStream());
-				 */
-				//while (true) {
-				for (int i = 0; i < listTests.size(); i++) {
-							String status;
-							if (listResults.get(i).wasSuccessful()) {
-								status = "OK";
-							}
-							else if (!listResults.get(i).wasSuccessful()) {
-								status = "KO";
-							}
-							else status = "Undefined";
-							
-							System.out.print(listTests.get(i) + "|" + status);
-							if (listTests.indexOf(listTests.get(i)) != listTests.size()-1) {
-								System.out.print(";");
-							}
-							// output.print(testName);
-							// output.print(testResult.wasSuccessful()); output.flush(); Thread.sleep(100);
-						}
-				//}
+				SerialPort serialPort = SerialPort.getCommPort("COM3");
+				serialPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
+				if (serialPort.openPort()) {
+					PrintWriter output = new PrintWriter(serialPort.getOutputStream());
+					for (int i = 0; i < listTests.size(); i++) {
+						String status;
+						if (listResults.get(i).wasSuccessful()) {
+							status = "ok";
+						} else if (!listResults.get(i).wasSuccessful()) {
+							status = "ko";
+						} else
+							status = "Undefined";
+						
+						//System.out.print(listTests.get(i) + "|" + status + ";");
+						/*if (listTests.indexOf(listTests.get(i)) != listTests.size() - 1) {
+							System.out.print(";");
+						}*/
 
+						message = message + listTests.get(i) + "|" + status + ";";
+						
+					}
+					String Test3 = "test3|ko;";
+					String Test4 = "test4|ok;";
+					String Test5 = "test5|ko;";
+					String Test6 = "test6|ko;";
+
+					message = message + Test3 + Test4 + Test5 + Test6;
+					System.out.println(message);
+					
+					Thread.sleep(2000);
+					output.print(message);
+					output.flush();
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
